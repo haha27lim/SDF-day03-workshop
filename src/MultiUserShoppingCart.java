d116fa51b0d17295520c2f63e448280f88f61528
haha27lim <eileen2708@hotmail.com>
2022-12-05 22:41:39 +0800

    day03 workshop

---
+++ src/MultiUserShoppingCart.java
@@ -0,0 +1,15 @@
+/**
+ * App
+ */
+
+public class MultiUserShoppingCart {
+
+    public static void main(String[] args) {
+
+    
+        ShoppingCartDB cart = new ShoppingCartDB("cartdb"); //default folder: "db"
+        // cart.setup();
+        cart.startShell();
+        
+    }
+}
